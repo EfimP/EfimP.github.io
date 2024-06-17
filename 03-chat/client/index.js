b5d6bd3bb6d0114768899bd371593fda00339ffc
PML30\cgsg202 <cgsg202@ad.pml30.ru>
2024-06-17 10:31:11 +0300

    Added chat

---
+++ 03-chat/client/index.js
@@ -0,0 +1,14 @@
+let socket = new WebSocket("ws://localhost:8000");
+
+socket.onopen = (event) =>{
+    console.log("Socket open");
+};
+
+socket.onmessage = (event) => {
+    $("#MsgArea").append(event.data);
+};
+
+export function OnMouseClick (text) {
+    socket.send(text);
+    console.log("Message:", text);
+}
